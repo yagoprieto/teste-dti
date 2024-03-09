dc3dbbc533ac6480e446dbd95bdefec1cd555678
yagoprieto <122551074+yagoprieto@users.noreply.github.com>
2024-03-09 19:16:15 -0300

    3 commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React, { useState } from 'react';
+import PetshopComponent from './petshop';
+
+
+function App() {
+return (
+  <div className="App">
+  <PetshopComponent/>
+  </div>
+    
+  );
+}
+
+export default App;
